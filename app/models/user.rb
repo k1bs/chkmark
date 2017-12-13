9f7ab100c246ecb78fe0e054c485ec7760412292
Alex Kibler <alexkibler@me.com>
2017-12-13 10:26:01 -0500

    update api and sessions controller, user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
   validates_uniqueness_of :username
   has_secure_password
+  has_secure_token :auth_token
+
+  def invalidate_token
+    update_columns(auth_token: nil)
+  end
+
+  def self.validate_login(username, password)
+    user = find_by(username: username)
+    user if user && user.authenticate(password)
+  end
 end
