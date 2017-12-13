e2aa56f3d2e6c6f1c77b8928155b0b797ec71e7b
Alex Kibler <alexkibler@me.com>
2017-12-13 11:56:58 -0500

    add basic dependencies to index.js, App.js, format both

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+
+const store = createStore('REDUCER GOES HERE')
+
+ReactDOM.render(<App store={store} />, document.getElementById('root'))
+registerServiceWorker()
