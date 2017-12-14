de6989dec378dcef081f141f1bc92d3845e5fc18
Alex Kibler <alexkibler@me.com>
2017-12-14 14:18:41 -0500

    define initial actions

---
+++ client/src/actions/index.js
@@ -0,0 +1,64 @@
+let nextNoteId = 0
+
+export const addNote = (text) => {
+  return {
+    type: 'ADD_NOTE',
+    id: nextNoteId++,
+    note: text
+  }
+}
+
+export const updateNote = (id, text) => {
+  return {
+    type: 'UPDATE_NOTE',
+    id,
+    note: text
+  }
+}
+
+export const deleteNote = (id) => {
+  return {
+    type: 'DELETE_NOTE',
+    id
+  }
+}
+
+export const logout = () => {
+  return {
+    type: 'LOGOUT'
+  }
+}
+
+export const login = (token, name) => {
+  return {
+    type: 'LOGIN',
+    token,
+    name
+  }
+}
+
+export const setCurrentNote = (id) => {
+  return {
+    type: 'SET_CURRENT_NOTE',
+    id
+  }
+}
+
+export const clearCurrentNote = () => {
+  return {
+    type: 'CLEAR_CURRENT_NOTE'
+  }
+}
+
+export const toggleEdit = () => {
+  return {
+    type: 'TOGGLE_EDIT'
+  }
+}
+
+export const fetchingNotes = (value) => {
+  return {
+    type: 'FETCHING_NOTES',
+    value
+  }
+}
