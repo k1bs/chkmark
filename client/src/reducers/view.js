a782d6bf161e59a8230a3202e776a60a3bd82d08
Alex Kibler <alexkibler@me.com>
2017-12-15 13:50:40 -0500

    change structure of view reducer

--- client/src/reducers/view.js
+++ client/src/reducers/view.js
@@ -1,15 +1,7 @@
 const view = (state = 'NOTE', action) => {
   switch (action.type) {
-    case 'VIEW_NOTE':
-      return 'NOTE'
-    case 'VIEW_LOGIN':
-      return 'LOGIN'
-    case 'VIEW_PROFILE':
-      return 'PROFILE'
-    case 'VIEW_REGISTER':
-      return 'REGISTER'
-    case 'VIEW_HOME':
-      return 'HOME'
+    case 'VIEW_MODE':
+      return action.value
     default:
       return state
   }
