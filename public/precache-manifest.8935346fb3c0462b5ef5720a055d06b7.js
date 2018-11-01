6848792dd8b9348ce50f949915c9b738d27500c5
Alex Kibler <alexkibler@me.com>
2018-11-01 13:22:23 -0600

    update dependencies to resolve security, redeploy

---
+++ public/precache-manifest.8935346fb3c0462b5ef5720a055d06b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "00a34854a412a3774bfd",
+    "url": "/static/js/main.00a34854.chunk.js"
+  },
+  {
+    "revision": "d111da9a704acf1ddd82",
+    "url": "/static/js/1.d111da9a.chunk.js"
+  },
+  {
+    "revision": "00a34854a412a3774bfd",
+    "url": "/static/css/main.1b3c10d3.chunk.css"
+  },
+  {
+    "revision": "ad2f25816c6e6bb5d53c5f864dbbd077",
+    "url": "/index.html"
+  }
+];
